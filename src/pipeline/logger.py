2dda2168ab5fa04e13da176ac7fe98b827080975
Debargha Mishra <debarghamishra@Debarghas-MacBook-Air.local>
2026-09-05 19:42:33 +0530

    EDA and problem Statement

--- src/pipeline/logger.py
+++ src/pipeline/logger.py
@@ -2,6 +2,7 @@ import logging
 import os
 from datetime import datetime
 
+
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
 logs_path = os.path.join(os.getcwd(), "logs")
@@ -14,6 +15,3 @@ logging.basicConfig(
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
-
-if __name__ == "__main__":
-    logging.info("Logging has started.")
